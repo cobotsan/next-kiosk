39a5059a299160e200175f2b35a0c2f05d7c0b4e
mamiaydin <muha2metaydin@gmail.com>
2025-06-14 01:28:43 +0300

    these files needed for gh pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/next-kiosk',
+  assetPrefix: '/next-kiosk/',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
